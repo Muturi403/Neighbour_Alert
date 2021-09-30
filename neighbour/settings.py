09a487c7b2626e564ef5a92ad651c9d09b307ca4
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 20:36:17 +0300

    this will add the django app

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['.localhost','.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'neigbourwatch',
+    'neighbourwatch',
     'bootstrap4',
     'tinymce',
     'django.contrib.admin',
