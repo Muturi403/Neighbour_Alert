0fd8d3381a8f02e878ab0df94a74436ab982e97f
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 21:15:50 +0300

    this will add the user, profile, business and post form

--- neighbourwatch/forms.py
+++ neighbourwatch/forms.py
@@ -1,3 +1,27 @@
 from django import forms
 from .models import Business, Post, Profile
 from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+  class Meta:
+    model = User
+    fields = ('username', 'email',)
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+      model = Profile
+      fields = '__all__'
+      exclude = ['username', 'count',]
+
+class BusinessForm(forms.ModelForm):
+  class Meta:
+    model = Business
+    fields = '__all__'
+    exclude = ['username', 'neighborhood', 'category']
+
+class PostiiForm(forms.ModelForm):
+  class Meta:
+    model = Post
+    fields = ('posttitle', 'story', )
+    exclude = ['postuser', 'neighborhood']
+    
