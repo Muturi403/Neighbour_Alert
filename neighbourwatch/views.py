daa9f50e40fc9e7a01a5513732545963c11aa53a
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 20:55:42 +0300

    this will add the login required

--- neighbourwatch/views.py
+++ neighbourwatch/views.py
@@ -7,3 +7,22 @@ from django.contrib import messages
 from django.http import JsonResponse
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    if request.user.profile.neighborhood == None:
+        messages.success(request, 'Please fill out you Neighbourhood')
+        return redirect('uprofile')
+    else:
+        neighdetails = Neighborhood.objects.get(
+            name=request.user.profile.neighborhood)
+        businesses = Business.objects.filter(
+            neighborhood=request.user.profile.neighborhood)
+        stories = Postii.objects.filter(
+            neighborhood=request.user.profile.neighborhood)
+
+        params = {
+            'neighdetails': neighdetails,
+            'businesses': businesses,
+            'stories': stories,
+        }
+        return render(request, 'index.html', params)
