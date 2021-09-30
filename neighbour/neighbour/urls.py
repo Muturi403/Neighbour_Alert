0941e13650fd719c791f89ea287f34dc531c1874
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 20:19:00 +0300

    this will add the url patterns

--- neighbour/neighbour/urls.py
+++ neighbour/neighbour/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('ghetto.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
