b696e22426063e961c70900f12123ef2a02bd667
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 20:21:03 +0300

    this will import django heroku, database and config

--- neighbour/neighbour/settings.py
+++ neighbour/neighbour/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
