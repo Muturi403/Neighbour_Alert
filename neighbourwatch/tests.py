333a507ac379e78e7488db6f0dae782f64aad720
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 21:04:17 +0300

    this will add the neighborhood and profile test

--- neighbourwatch/tests.py
+++ neighbourwatch/tests.py
@@ -16,3 +16,51 @@ class TestLocation(TestCase):
   def test_save_location(self):
     self.location2 = Location.objects.create(location='Nairobi')
     self.assertEqual(len(Location.objects.all()), 2)
+    
+  class TestNeighborhodd(TestCase):
+	  def setUp(self):
+    self.location = Location.objects.create(location='Machakos')
+    self.hood = Neighborhood.objects.create(name='Kisumu Ndogo', location=self.location, policehelpline=2, hospitalhelpline=2, occupants=8)
+
+
+  def tearDown(self):
+    Location.objects.all().delete()
+    Neighborhood.objects.all().delete()
+
+  def test_isinstance(self):
+    self.assertTrue(isinstance(self.hood, Neighborhood))
+
+  def test_deletehood(self):
+    self.hood2 = Neighborhood.objects.create(name='Embakasi', location=self.location, policehelpline= 2, hospitalhelpline=4, occupants=8)
+    self.assertEqual(len(Neighborhood.objects.all()),2)
+    Neighborhood.delete_neigborhood(self.hood.id)
+    self.assertEqual(len(Neighborhood.objects.all()),1)
+
+  def test_findneighborhood(self):
+    searchterm = 'Embakasi'
+    self.hood2 = Neighborhood.objects.create(name='Embakasi', location=self.location, policehelpline= 2, hospitalhelpline=4, occupants=8)
+    results = Neighborhood.find_neigborhood(searchterm)
+    self.assertTrue(len(results), 1)
+
+  def test_updateneighbor(self):
+    self.hood2 = Neighborhood.objects.create(name='Embakasi', location=self.location, policehelpline= 2, hospitalhelpline=4, occupants=8)
+    Neighborhood.update_neighborhood(self.hood2.id, name='kajiado')
+    updated = Neighborhood.objects.get(id = self.hood2.id)
+    self.assertEqual(updated.name, 'kajiado')
+
+class TestProfile(TestCase):
+  def setUp(self):
+    self.newuser = User(username = "maingi")
+    self.newuser.save()
+    self.location = Location.objects.create(location='Machakos')
+    self.hood = Neighborhood.objects.create(name='Kisumu Ndogo', location=self.location, policehelpline= 2, hospitalhelpline=4, occupants=8)
+    self.newprofile = Profile.objects.create(profilePic='test.jpg', bio='i am amazing', phone=2, location=self.location, neighborhood=self.hood )
+
+  def tearDown(self):
+    Profile.objects.all().delete()
+    User.objects.all().delete()
+    Neighborhood.objects.all().delete()
+    Location.objects.all().delete
+
+  def test_isinstance(self):
+    self.assertTrue(isinstance(self.newprofile, Profile))  
