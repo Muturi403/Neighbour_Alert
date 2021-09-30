ff60e9aafea5bdae12f0869d8b8b64b1a4a02952
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 21:06:06 +0300

    this will add the post tests

--- neighbourwatch/tests.py
+++ neighbourwatch/tests.py
@@ -97,4 +97,28 @@ class TestProfile(TestCase):
     searchresults = Business.searchbusiness(searchterm)
     self.assertEqual(len(searchresults), 1)
 
-  
+ 
+class Post(TestCase):
+  def setUp(self):
+    self.location = Location.objects.create(location='Machakos')
+    self.hood = Neighborhood.objects.create(name='Kisumu Ndogo', location=self.location, policehelpline='00022233211', hospitalhelpline='0092133313', occupants=8)
+    self.post = Postii.objects.create(posttitle='theives found', story='wezi wa tatu walipatikana', neighborhood=self.hood)
+
+  def tearDown(self):
+    Location.objects.all().delete()
+    User.objects.all().delete()
+    Neighborhood.objects.all().delete()
+    Postii.objects.all().delete
+
+  def test_isinstance(self):
+    self.assertTrue(isinstance(self.post, Postii))
+
+  def test_save_post(self):
+    self.post2 = Postii.objects.create(posttitle='wezi walipatikana ', story='thieves were found', neighborhood=self.hood)
+    self.assertEqual(len(Postii.objects.all()), 2)
+
+  def test_delete_post(self):
+    self.post2 = Postii.objects.create(posttitle='wezi walipatikana ', story='thieves were found', neighborhood=self.hood)
+    self.assertEqual(len(Postii.objects.all()), 2)
+    Postii.delete_post(self.post2.id)
+    self.assertEqual(len(Postii.objects.all()), 1)  
