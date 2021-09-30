1c391f9e585db96ecc2da7216a754258e4e720bb
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 21:07:56 +0300

    this will add the location and category model

--- neighbourwatch/models.py
+++ neighbourwatch/models.py
@@ -5,3 +5,17 @@ from django.db.models.signals import post_save
 from django.db.models import Q
 
 # Create your models here.
+class Location(models.Model):
+  location = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.location
+
+  def save_location(self):
+    self.save()
+
+class Category(models.Model):
+  category = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.category
