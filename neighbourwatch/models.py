3cbf4d30351146fd40a322554b2b18641d40a68c
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 21:07:03 +0300

    this will add the django imports in the models file

--- neighbourwatch/models.py
+++ neighbourwatch/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.db.models import Q
 
 # Create your models here.
