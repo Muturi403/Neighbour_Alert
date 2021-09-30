9ce888b6dd4df7a9226ff3789d54f2b14d0caa38
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 20:53:00 +0300

    this will add importations in the view file

--- neighbourwatch/views.py
+++ neighbourwatch/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Postii, Business, Neighborhood, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import UserForm, ProfileForm, BusinessForm, PostiiForm
+from django.contrib import messages
+from django.http import JsonResponse
 
 # Create your views here.
