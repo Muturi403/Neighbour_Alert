8d4bc182a800414fb62b3abdceb3e24c5ca83bc6
Muturi403 <faith.muturi@student.moringaschool.com>
2021-09-30 21:03:04 +0300

    this will add the location tests

--- neighbourwatch/tests.py
+++ neighbourwatch/tests.py
@@ -3,3 +3,16 @@ from .models import Business, Location, Profile, Neighborhood, Postii
 from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestLocation(TestCase):
+  def setUp(self):
+    self.location = Location.objects.create(location='Machakos')
+
+  def tearDown(self):
+    Location.objects.all().delete()
+
+  def test_isinstance(self):
+    self.assertTrue(isinstance(self.location, Location))
+
+  def test_save_location(self):
+    self.location2 = Location.objects.create(location='Nairobi')
+    self.assertEqual(len(Location.objects.all()), 2)
